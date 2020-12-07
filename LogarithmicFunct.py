19c7a65b0dd27b6773c57c6ad2f2547371ea8b42
TheShiferawShow <68077955+TheShiferawShow@users.noreply.github.com>
2020-12-06 22:06:37 -0500

    Create LogarithmicFunct.py

---
+++ LogarithmicFunct.py
@@ -0,0 +1,7 @@
+def funct(p):
+    import math #import math function
+
+    r = float(math.log(p - 10)) / (math.log(5))
+    r = float(math.log(r))
+    r = float(r / (math.log(2)))
+    print('The solution is ', r)  #print result
